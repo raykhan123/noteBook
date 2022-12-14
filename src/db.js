eda90667f31c200f9115a264f35077e71c71880e
raykhan123 <raykhan.rk123@gmail.com>
2022-12-14 19:07:47 +0530

    done

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
-const mongoose =require("mongoose")
-const mongooseUri= "mongodb+srv://danish123:Faizan123450@cluster0.sk9syp9.mongodb.net/notebook"
-const connectToMongo =()=>{
-    mongoose.connect(mongooseUri, {
-        useNewUrlParser: true
-    })
-    .then(()=>console.log("mongoDB is connected"))
-    .catch((error)=>console.log(error))
+// const mongoose =require("mongoose")
+// const mongooseUri= "mongodb+srv://danish123:Faizan123450@cluster0.sk9syp9.mongodb.net/notebook"
+// const connectToMongo =()=>{
+//     mongoose.connect(mongooseUri, {
+//         useNewUrlParser: true
+//     })
+//     .then(()=>console.log("mongoDB is connected"))
+//     .catch((error)=>console.log(error))
     
-}
-module.exports = connectToMongo
+// }
+// module.exports = connectToMongo
--- src/index.js
+++ src/index.js
@@ -11,13 +11,20 @@ app.use(cors())
 
 
 app.use(bodyParser.json())
-connectToMongo()
+// connectToMongo()
 
 // app.get('/',(req,res)=>{
 // res.send("My name is Danish")
 // })
-app.use('/',route)
-app.listen(process.env.PORT || 5000,function(){
-    console.log("express app is running on PORT " + (process.env.PORT || 5000))
+mongoose.connect("mongodb+srv://danish123:Faizan123450@cluster0.sk9syp9.mongodb.net/notebook", {
+    useNewUrlParser: true })
+.then(() => console.log("mongoDB is connected"))
+.catch((err) => console.log(err));
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port, function () {
+    console.log("app is running on the port" + port)
 })
 
