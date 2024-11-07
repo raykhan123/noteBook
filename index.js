c490a54cd91820f2a1a3c8f3e440ea2427391e6d
Raykhan <raykhan.rk123@gmail.com>
2024-11-07 08:32:15 +0530

    vercel json added

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const route = require("./src/routes/route"); // Import routes
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(bodyParser.json()); // Middleware to parse JSON
+// app.use('/', route); // Use the router for all routes
+// app.get('/', (req, res) => res.send('Server is up on UAT'))
+
+// mongoose.connect(process.env.Mongo_String, {
+//   useNewUrlParser: true,
+// })
+// .then(() => console.log("MongoDB is connected"))
+// .catch((err) => console.log(err));
+
+// // Test route
+// app.get('/', (req, res) => res.send('Hello World'));
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log("App is running on port " + port);
+// });
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const route = require("./src/routes/route"); // Import routes
+const route = require("./routes/route"); // Import routes
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json()); // Middleware to parse JSON
 app.use('/', route); // Use the router for all routes
-app.get('/', (req, res) => res.send('Server is up on UAT'))
+app.get('/', (req, res) => res.send('Server is up on UAT'));
 
 mongoose.connect(process.env.Mongo_String, {
   useNewUrlParser: true,
@@ -20,9 +50,7 @@ mongoose.connect(process.env.Mongo_String, {
 .catch((err) => console.log(err));
 
 // Test route
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/test', (req, res) => res.send('Hello World'));
 
-// Start the server
-app.listen(port, () => {
-  console.log("App is running on port " + port);
-});
+// Export the app as a module for Vercel
+module.exports = app;
